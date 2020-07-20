cdb0c48e97e6402df5f6f5829c374955b3febcdc
snk <ivan.zakirov@gmail.com>
2020-07-20 21:41:41 +0500

    Почистил лишнее.

---
+++ KnowledgeTesting/BL/QuestionManagement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnowledgeTesting.BL
+{
+	/// <summary>
+	/// Управление вопросами.
+	/// </summary>
+	public class QuestionManagement
+	{
+		DB.PgSql.DbPgSqlContext _DbContext = new DB.PgSql.DbPgSqlContext();
+	}
+}
