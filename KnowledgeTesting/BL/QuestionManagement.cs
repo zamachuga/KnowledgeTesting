4fa2a0eeb984205b7a365521f37e11b08562a907
snk <ivan.zakirov@gmail.com>
2020-07-20 23:39:14 +0500

    Максимальное добавление ответов в тест.

--- KnowledgeTesting/BL/QuestionManagement.cs
+++ KnowledgeTesting/BL/QuestionManagement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity.SqlServer;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
 
 namespace KnowledgeTesting.BL
 {
@@ -24,16 +25,38 @@ namespace KnowledgeTesting.BL
 			_DbContext.Answers.Add(Answer);
 			_DbContext.SaveChanges();
 		}
-		
+
 		public DAO.Answer GetAnswer(int Id)
 		{
 			return _DbContext.Answers.Find(Id);
 		}
 
+		/// <summary>
+		/// Добавить вариант ответа в вопрос.
+		/// </summary>
+		/// <param name="Question"></param>
+		/// <param name="Answer"></param>
+		public void AddAnswer(DAO.Question Question, DAO.Answer Answer)
+		{
+			if ((Question.Answers.Count() >= 3)) throw new Exception("Вопрос может содержать не более 3 вариантов ответа.");
+
+			Question.Answers.Add(Answer);
+		}
+
+		/// <summary>
+		/// Установить правильный ответ на вопрос.
+		/// </summary>
+		public void SetCorrectAnswer(DAO.Question Question, DAO.Answer Answer)
+		{
+			if (!Question.Answers.Contains(Answer)) throw new Exception("Правильный ответ должен быть одним из вариантов ответов.");
+
+			Question.CorrectAnswer = Answer;
+		}
+
 		private bool IsExist(DAO.Answer Answer)
 		{
 			DAO.Answer _FinKey = _DbContext.Answers.Find(Answer.Id);
-			int _FindText = _DbContext.Answers.Where(x => x.Text.ToLower().Replace(" ","") == Answer.Text.ToLower().Replace(" ", "")).Count();
+			int _FindText = _DbContext.Answers.Where(x => x.Text.ToLower().Replace(" ", "") == Answer.Text.ToLower().Replace(" ", "")).Count();
 
 			bool _IsExist = _FinKey != null || _FindText > 0;
 			return _IsExist;
