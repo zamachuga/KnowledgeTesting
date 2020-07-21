fc4d35bf6ce2c968b2ffd104f7cfd83abad23fba
snk <ivan.zakirov@gmail.com>
2020-07-22 01:21:59 +0500

    Управление тестируемыми. Создание объекта.

---
+++ KnowledgeTesting/BL/InterviweeManagement.cs
@@ -0,0 +1,26 @@
+﻿using KnowledgeTesting.BL.DB.PgSql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnowledgeTesting.BL
+{
+	/// <summary>
+	/// Управление тестируемыми.
+	/// </summary>
+	public class InterviweeManagement
+	{
+		DbPgSqlContext _DbContext = DbPgSqlContext.Instance();
+
+		public void CreateInterviwee(DAO.Interviwee Interviwee) {
+			if (string.IsNullOrEmpty(Interviwee.FirstName)) throw new Exception("Пустое имя.");
+			if (string.IsNullOrEmpty(Interviwee.LasName)) throw new Exception("Пустая фамилия.");
+
+			_DbContext.Interviwees.Add(Interviwee);
+			_DbContext.SaveChanges();
+		}
+
+
+	}
+}
