f15617b5d31ca87aa40d42beb73706bfd50a9771
snk <ivan.zakirov@gmail.com>
2020-07-21 22:40:31 +0500

    Файл миграции БД.

---
+++ KnowledgeTesting/Migrations/202007211738346_TestQuestionsMTM.cs
@@ -0,0 +1,45 @@
+﻿namespace KnowledgeTesting.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TestQuestionsMTM : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.TestQuestions",
+                c => new
+                    {
+                        TestId = c.Int(nullable: false),
+                        QuestionId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.TestId, t.QuestionId })
+                .ForeignKey("dbo.Questions", t => t.QuestionId, cascadeDelete: true)
+                .ForeignKey("dbo.Tests", t => t.TestId, cascadeDelete: true)
+                .Index(t => t.TestId)
+                .Index(t => t.QuestionId);
+            
+            CreateTable(
+                "dbo.Tests",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.TestQuestions", "TestId", "dbo.Tests");
+            DropForeignKey("dbo.TestQuestions", "QuestionId", "dbo.Questions");
+            DropIndex("dbo.TestQuestions", new[] { "QuestionId" });
+            DropIndex("dbo.TestQuestions", new[] { "TestId" });
+            DropTable("dbo.Tests");
+            DropTable("dbo.TestQuestions");
+        }
+    }
+}
