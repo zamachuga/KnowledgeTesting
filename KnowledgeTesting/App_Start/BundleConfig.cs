9d1e8ba6ab6609d504431d8fede7e419db9049ae
snk <ivan.zakirov@gmail.com>
2020-07-23 01:09:42 +0500

    Настройка загрузки крипта Js - исполняет ядро работы сайта и запросов к контроллеру.

--- KnowledgeTesting/App_Start/BundleConfig.cs
+++ KnowledgeTesting/App_Start/BundleConfig.cs
@@ -10,18 +10,18 @@ namespace KnowledgeTesting
 		{
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
 									"~/Scripts/jquery-{version}.js"));
+			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+								"~/Scripts/bootstrap.js"));
+			bundles.Add(new ScriptBundle("~/bundles/AjaxCore").Include(
+					"~/Scripts/AjaxCore/SiteCore.js.js"));
+
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
 									"~/Scripts/jquery.validate*"));
-
 			// Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
 			// готово к выпуску, используйте средство сборки по адресу https://modernizr.com, чтобы выбрать только необходимые тесты.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 									"~/Scripts/modernizr-*"));
-
-			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-								"~/Scripts/bootstrap.js"));
-
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 								"~/Content/bootstrap.css",
 								"~/Content/site.css"));
