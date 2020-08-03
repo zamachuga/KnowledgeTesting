83c15ec8aa57b5f079d803d9f0604b5794d2fcd9
snk <ivan.zakirov@gmail.com>
2020-08-04 00:04:55 +0500

    IStatistic

---
+++ KnowledgeTesting/BL/IStatistic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeTesting.BL
+{
+
+	/// <summary>
+	/// Статистика.
+	/// </summary>
+	public interface IStatistic
+	{
+		/// <summary>
+		/// Количество прохождений теста.
+		/// </summary>
+		/// <param name="Interviwee"></param>
+		/// <param name=""></param>
+		/// <returns></returns>
+		int GetCountCompleteTest(int InterviweeId, int TestId);
+
+		/// <summary>
+		/// Получить многомерный массив статистики вопросов.
+		/// 0 - номера вопросов.
+		/// 1 - правильных ответов на вопросы.
+		/// 2 - неправильных ответов на вопросы.
+		/// </summary>
+		int[][] GetArrayQuestionStatistic(int TestId);
+	}
+}
